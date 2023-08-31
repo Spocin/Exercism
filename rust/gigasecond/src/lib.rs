219968ef89a93785de96fbfc7b54189d6259d898
Spocin <spocinomaniak@gmail.com>
2023-08-31 23:39:57 +0200

    Moved GIGA_SECOND to const

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,6 +1,9 @@
-use time::{Duration, PrimitiveDateTime as DateTime};
+use time::PrimitiveDateTime as DateTime;
+use time::ext::NumericalDuration;
+
+const GIGA_SECOND: i64 = 1e9 as i64;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    return start.checked_add(Duration::seconds(1000000000)).unwrap();
+    return start.checked_add(GIGA_SECOND.seconds()).unwrap();
 }
