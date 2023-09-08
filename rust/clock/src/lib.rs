179fe2f8a740f266e13aedc4b2f68292354039d6
michal_spocinski <michal.spocinski.01@gmail.com>
2023-09-08 11:15:08 +0200

    Moved logic to process fn. Handled negative numbers

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -8,26 +8,11 @@ pub const DAY_AS_MINUTES: i32 = 1440;
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        return Clock {
-            hours,
-            minutes,
-        };
+        return Clock::process(hours, minutes);
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let mut clock_as_minutes = (self.hours * 60) + self.minutes + minutes;
-
-        if clock_as_minutes >= DAY_AS_MINUTES {
-            clock_as_minutes = clock_as_minutes % DAY_AS_MINUTES;
-        }
-
-        let hour = clock_as_minutes / 60;
-        let minute = clock_as_minutes - (hour * 60);
-
-        return Self {
-            hours: hour,
-            minutes: minute,
-        }
+        return Clock::process(self.hours, self.minutes + minutes)
     }
 
     pub fn to_string(&self) -> String {
@@ -53,6 +38,29 @@ impl Clock {
     }
 
     pub fn eq(&self, other: &Self) -> bool {
-        return self.hours == other.hours;
+        self.hours == other.hours && self.minutes == other.minutes
+    }
+
+    fn process (hours: i32, minutes: i32) -> Self {
+        let mut clock_as_minutes = (hours * 60) + minutes;
+
+        /* Handle negative values */
+        if clock_as_minutes < 0 {
+            clock_as_minutes = clock_as_minutes % DAY_AS_MINUTES;
+            clock_as_minutes = DAY_AS_MINUTES - (clock_as_minutes * -1);
+        }
+
+        /* Normalize to day */
+        if clock_as_minutes >= DAY_AS_MINUTES {
+            clock_as_minutes = clock_as_minutes % DAY_AS_MINUTES;
+        }
+
+        let hour = clock_as_minutes / 60;
+        let minute = clock_as_minutes - (hour * 60);
+
+        return Self {
+            hours: hour,
+            minutes: minute,
+        }
     }
 }
