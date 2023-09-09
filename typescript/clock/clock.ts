51f1b5d4210392a91aab7e1656b2eda6aa121d5d
michal_spocinski <michal.spocinski.01@gmail.com>
2023-09-09 16:58:07 +0200

    Work snapshot

--- typescript/clock/clock.ts
+++ typescript/clock/clock.ts
@@ -1,10 +1,24 @@
 export class Clock {
-  constructor(hour: unknown, minute?: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  private DAY_AS_MINUTES = 1440;
+  private minutes: number;
+
+  constructor(hour: number, minute?: number) {
+    let tmpMinutes =  ((hour * 60) + (minute ?? 0));
+    if (tmpMinutes >= this.DAY_AS_MINUTES) tmpMinutes = tmpMinutes % this.DAY_AS_MINUTES;
+    this.minutes = tmpMinutes;
   }
 
-  public toString(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public toString(): string {
+    const opt: Intl.NumberFormatOptions = {
+      minimumIntegerDigits: 2,
+      maximumFractionDigits: 0,
+      useGrouping: false,
+    }
+
+    const hours = (this.minutes / 60).toLocaleString('pl-PL', opt);
+    const minutes = (this.minutes % 60).toLocaleString('pl-PL', opt);
+
+    return `${hours}:${minutes}`
   }
 
   public plus(minutes: unknown): Clock {
