49e5373697f72a6d30c2120d73346a5761498f7e
michal_spocinski <michal.spocinski.01@gmail.com>
2023-09-10 14:48:14 +0200

    Refactored robot-name to generate names on fly.

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,30 +1,10 @@
 export class Robot {
-  private static readonly namesArr: string[] = [];
+  private static usedNamesSet = new Set<string>();
 
   private _name: string;
-  private usedNamesSet = new Set<string>();
-
-  private static initializeNamesMap() {
-    const opt: Intl.NumberFormatOptions = {
-      minimumIntegerDigits: 3,
-      useGrouping: false,
-    }
-
-    for (let i = 65; i < 91; i++) {
-      for (let j = 65; j < 91; j++) {
-        for (let k = 0; k <= 999; k++) {
-          Robot.namesArr.push(
-              `${String.fromCharCode(i)}${String.fromCharCode(j)}${k.toLocaleString('pl-PL', opt)}`
-          )
-        }
-      }
-    }
-  }
 
   constructor() {
-    if (!Robot.namesArr.length) Robot.initializeNamesMap();
-    this._name = this.getRandomName();
-    this.usedNamesSet.add(this._name);
+    this._name = this.findRandomName();
   }
 
   public get name(): string {
@@ -32,24 +12,42 @@ export class Robot {
   }
 
   public resetName(): void {
-    let newName = this.getRandomName();
-
-    while (this.usedNamesSet.has(newName)) {
-      Robot.namesArr.push(newName);
-      newName = this.getRandomName();
-    }
-
-    this._name = newName;
+    this._name = this.findRandomName();
   }
 
   public static releaseNames(): void {
-    Robot.namesArr.length = 0;
+    Robot.usedNamesSet.clear();
+  }
+
+  private findRandomName(): string {
+    let newName: string;
+
+    do {
+      newName = this.getRandomName();
+    } while (Robot.usedNamesSet.has(newName))
+
+    Robot.usedNamesSet.add(newName);
+
+    return newName;
   }
 
   private getRandomName(): string {
-    const chosenIdx = Math.floor(Math.random() * Robot.namesArr.length);
-    const choseName = Robot.namesArr[chosenIdx];
-    Robot.namesArr.splice(chosenIdx, 1);
-    return choseName;
+    const opt: Intl.NumberFormatOptions = {
+      minimumIntegerDigits: 3,
+      useGrouping: false,
+    }
+
+    const firstChar = this.getRandomNumFromRange('A', 'Z');
+    const secondChar = this.getRandomNumFromRange('A', 'Z');
+    const num = Math.floor(Math.random() * 999);
+
+    return `${firstChar}${secondChar}${num.toLocaleString('pl-PL', opt)}`;
+  }
+
+  private getRandomNumFromRange(min: string, max: string) {
+    const minAsNum = min.charCodeAt(0);
+    const maxAsNum = max.charCodeAt(0);
+    const randomNum = Math.floor(Math.random() * (maxAsNum + 1 - minAsNum) + minAsNum);
+    return String.fromCharCode(randomNum);
   }
 }
