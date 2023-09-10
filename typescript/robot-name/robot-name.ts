0af4be49cc45e9ba2165541363d6417fbaf1ad8d
michal_spocinski <michal.spocinski.01@gmail.com>
2023-09-10 12:25:50 +0200

    Implemented generation of namesMap

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,5 +1,27 @@
 export class Robot {
-  constructor() {}
+  private static namesMap = new Map<string, boolean>();
+
+  private static initializeNamesMap() {
+    const opt: Intl.NumberFormatOptions = {
+      minimumIntegerDigits: 3,
+      useGrouping: false,
+    }
+
+    for (let i = 65; i < 91; i++) {
+      for (let j = 65; j < 91; j++) {
+        for (let k = 0; k <= 999; k++) {
+          this.namesMap.set(
+              `${String.fromCharCode(i)}${String.fromCharCode(j)}${k.toLocaleString('pl-PL', opt)}`,
+              false,
+          )
+        }
+      }
+    }
+  }
+
+  constructor() {
+    if (!Robot.namesMap.size) Robot.initializeNamesMap();
+  }
 
   public get name(): string {
     throw new Error('Implement Robot#name')
