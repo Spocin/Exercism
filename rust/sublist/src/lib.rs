ac4a0996bc6df7daca57e9d0b4904e0c73873500
michal_spocinski <michal.spocinski.01@gmail.com>
2023-09-24 21:47:18 +0200

    Refactored to matcher solution

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,57 +7,22 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    if is_arr_a_equal_to_arr_b(_first_list, _second_list) { return Comparison::Equal; }
-    if is_a_sublist_of_b(_first_list,_second_list) { return Comparison::Sublist; }
-    if is_a_superlist_of_b(_first_list,_second_list) { return Comparison::Superlist; }
+    use Comparison::*;
 
-    Comparison::Unequal
-}
-
-fn is_arr_a_equal_to_arr_b<T: PartialEq>(a: &[T], b: &[T]) -> bool {
-    if a.len() != b.len() { return false }
-
-    a == b
-}
-
-fn is_a_sublist_of_b<T: PartialEq>(a: &[T], b: &[T]) -> bool {
-    if a.len() == 0 { return true }
-
-    let a_deduped = dedup_array(&a);
-    let b_deduped = dedup_array(&b);
-
-    //Find all idx to check for
-    let found_idx_vec = b_deduped.iter()
-        .enumerate()
-        .filter(|(_, &el)| el == a_deduped[0])
-        .map(|(idx, _)| idx)
-        .collect::<Vec<_>>();
-
-    if found_idx_vec.len() == 0 { return false; }
-
-    //Check for each
-    for idx in found_idx_vec {
-        let trailing_len_of_b = b_deduped.len() - idx;
-        if a_deduped.len() > trailing_len_of_b { continue }
-
-        let b_idx_to_slice_to = idx + a_deduped.len();
-        let b_slice_to_check = &b_deduped[idx..b_idx_to_slice_to];
-
-        if is_arr_a_equal_to_arr_b(&a_deduped, b_slice_to_check) { return true; }
+    match (_first_list.len(), _second_list.len()) {
+        (0,0) => Equal,
+        (0,_) => Sublist,
+        (_,0) => Superlist,
+        (a,b) if a > b => if _first_list
+            .windows(b)
+            .any(|el| el == _second_list) { Superlist } else { Unequal },
+        (a,b) if a < b => if _second_list
+            .windows(a)
+            .any(|el| el == _first_list) { Sublist } else { Unequal },
+        (_,_) => if _first_list == _second_list {
+            Equal
+        } else {
+            Unequal
+        }
     }
-
-    false
-}
-
-fn is_a_superlist_of_b<T: PartialEq>(a: &[T], b: &[T]) -> bool {
-    if b.len() == 0 { return true; }
-
-    is_a_sublist_of_b(&b, &a)
-}
-
-fn dedup_array<T: PartialEq>(arr: &[T]) -> Vec<&T> {
-    let mut vec_from_arr = arr.iter().collect::<Vec<_>>();
-    vec_from_arr.dedup();
-
-    vec_from_arr
 }
