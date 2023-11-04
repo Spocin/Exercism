60a7d6d8f28aecdd641f97d26e8281031f4418af
michal_spocinski <michal.spocinski.01@gmail.com>
2023-11-04 21:58:00 +0100

    Base logic

--- rust/minesweeper/src/lib.rs
+++ rust/minesweeper/src/lib.rs
@@ -1,3 +1,32 @@
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    todo!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{minefield:#?}\n");
+    let height = minefield.len();
+    let width = minefield[0].len();
+
+    let computed_minefield: Vec<Vec<i32>> = vec![vec![0; width]; height];
+
+    for (y, row) in minefield.iter().enumerate() {
+        for (x, field) in row.chars().enumerate() {
+            if field == '*' {
+                increment_around_bomb(&x, &y, &computed_minefield);
+            }
+        }
+    }
+
+    return computed_minefield
+        .iter()
+        .map(|row|
+            row.iter()
+                .map(|field|
+                    match field {
+                        &field if field == -1 => '*'.to_string(),
+                        &field if field == 0 => '0'.to_string(),
+                        &field => field.to_string()
+                    }
+                ).collect()
+        )
+        .collect();
+}
+
+fn increment_around_bomb(x: &usize, y: &usize, minefield: &Vec<Vec<i32>>) {
+
 }
