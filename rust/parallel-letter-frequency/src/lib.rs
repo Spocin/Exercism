f0da385dd493285181c6c836b32af6dfde3bdfb3
michal_spocinski <michal.spocinski.01@gmail.com>
2023-11-24 02:00:14 +0100

    Building solution

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -3,24 +3,25 @@ use std::thread;
 use std::thread::JoinHandle;
 
 pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
-    let mut worker_count_computed: &usize = &input.len();
+    let workers_to_spawn: usize;
     let mut words_count: HashMap<char, usize> = HashMap::new();
 
     match worker_count {
-        0 => worker_count_computed = &1,
-        _ => worker_count_computed = &worker_count,
+        0 => workers_to_spawn = 1,
+        x if x > input.len() => workers_to_spawn = input.len(),
+        _ => workers_to_spawn = worker_count,
     }
 
     let mut threads: Vec<JoinHandle<HashMap<char, usize>>> = vec![];
 
-    input.chunks(*worker_count_computed)
-        .for_each(|chunk| threads.push(spawn_worker_and_proccess(chunk)));
+    input.chunks(workers_to_spawn)
+        .for_each(|chunk| threads.push(spawn_worker_and_process(chunk)));
 
     for thread in threads {
         let chunk_result = thread.join();
 
         match chunk_result {
-            Ok(words_map) => { words_count.extend(words_map) }
+            Ok(words_map) => { concat_maps(&mut words_count, words_map) }
             Err(_) => { println!("Chunk failed!") }
         }
     }
@@ -28,18 +29,19 @@ pub fn frequency(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
     return words_count;
 }
 
-fn spawn_worker_and_proccess(chunk: &[&str]) -> JoinHandle<HashMap<char, usize>> {
+fn spawn_worker_and_process(chunk: &[&str]) -> JoinHandle<HashMap<char, usize>> {
     let chunk_owned: Vec<String> = chunk
         .iter()
+        .filter(|string| !string.is_empty())
         .map(|line| line.to_lowercase())
         .collect();
 
     return thread::spawn(move || {
         let mut chunk_words_count = HashMap::new();
 
-        for line in chunk_owned {
-            chunk_words_count.extend(count_chars(&line));
-        }
+        chunk_owned
+            .iter()
+            .for_each(|line| concat_maps(&mut chunk_words_count, count_chars(&line)));
 
         return chunk_words_count;
     })
@@ -55,4 +57,16 @@ fn count_chars(input: &String) -> HashMap<char, usize> {
         });
 
     return words_count;
+}
+
+fn concat_maps(a: &mut HashMap<char, usize>, b: HashMap<char, usize>) {
+    b.iter()
+        .for_each(|(key, val)| {
+            let anchor = a.get_mut(key);
+
+            match anchor {
+                Some(anchor_val) => { *anchor_val += val; }
+                None => { a.insert(*key, *val); }
+            }
+        })
 }
